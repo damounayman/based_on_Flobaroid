acc7240ec9cbb8a86094d76e8323d92382eae485
Jonathan Savin <jonathan.savin@inrs.fr>
2020-07-23 09:18:14 +0200

    Add comments in code to help future reading and change

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # based_on_Flobaroid
 This repository is based on Flobaroid to identify the dynamic parameters of abb irb 2600 and abb irb 6640
+
+##Content
+Six Python scripts are available :
+- `trajectory.py` This script computes an optimal excitation trajectory.
+- `visualizer.py` /!\\ TODO
+- `excite.py` /!\\ TODO
+- `identify.py` /!\\ TODO
+- `optimal_trajectory_test.py` /!\\ TODO
+- `plot trajectory.py` /!\\ TODO
+
+See the corresponding paragraph for description
+
+##`trajectory.py`
+This script computes an optimal excitation trajectory from the following usual parameters :
+
+1.  *\--filename* : the name of the file into which trajectory parameters are to be saved, in [**npz**](https://numpy.org/doc/stable/reference/generated/numpy.savez.html) format;
+
+1. *\--config* : the configuration options for trajectory optimization (algorithm, number of pass, etc.) in [**yaml**](https://yaml.org/) format;
+
+1. *\--model* : the file describing the robot considered, in [**urdf**](http://wiki.ros.org/urdf) format;
+
+1. *\--world* : the file describing the world in which the robot is set, in [**urdf**](http://wiki.ros.org/urdf) format.
+
+Additional parameters exist, see the Python code.
--- trajectory.py
+++ trajectory.py
@@ -15,6 +15,9 @@ from excitation.trajectoryGenerator import PulsedTrajectory
 from excitation.trajectoryOptimizer import TrajectoryOptimizer, simulateTrajectory
 from excitation.postureOptimizer import PostureOptimizer
 
+#=============================================================
+# Command Line Interface features : add parameters management 
+#=============================================================
 import argparse
 parser = argparse.ArgumentParser(description='Generate excitation trajectories, save to <filename>.')
 parser.add_argument('--filename', type=str, help='the filename to save the trajectory to, otherwise <model>.trajectory.npz')
@@ -24,6 +27,10 @@ parser.add_argument('--model_real', required=False, type=str, help='the file to 
 parser.add_argument('--world', required=False, type=str, help='the file to load world links from')
 args = parser.parse_args()
 
+#======================================
+# Algorithm configuration : add parser
+# and get options for execution
+#======================================
 import yaml
 with open(args.config, 'r') as stream:
     try:
@@ -36,14 +43,19 @@ config['urdf_real'] = args.model_real
 if config['useStaticTrajectories'] and not config['urdf_real']:
     print('When optimizing static postures, need model_real argument!')
     sys.exit()
+
 config['jointNames'] = iDynTree.StringVector([])
 if not iDynTree.dofsListFromURDF(config['urdf'], config['jointNames']):
     sys.exit()
+
 config['num_dofs'] = len(config['jointNames'])
 config['skipSamples'] = 0
 
+#===================================
+# Main : compute optimal trajectory 
+#===================================
 def main():
-    # save either optimized or random trajectory parameters to filename
+    # get output filename to save optimized or random trajectory parameters
     if args.filename:
         traj_file = args.filename
     else:
@@ -54,23 +66,42 @@ def main():
         old_sim = config['simulateTorques']
         config['simulateTorques'] = True
         model = Model(config, config['urdf'])
+
+        # set identification mode and parameters up
         if config['useStaticTrajectories']:
             old_gravity = config['identifyGravityParamsOnly']
-            idf = Identification(config, config['urdf'], config['urdf_real'], measurements_files=None,
-                                 regressor_file=None, validation_file=None)
-            trajectoryOptimizer = PostureOptimizer(config, idf, model, simulation_func=simulateTrajectory, world=args.world)
+            idf = Identification(config,
+                                 config['urdf'],
+                                 config['urdf_real'],
+                                 measurements_files=None,
+                                 regressor_file=None,
+                                 validation_file=None)
+            trajectoryOptimizer = PostureOptimizer(config,
+                                                   idf,
+                                                   model,
+                                                   simulation_func=simulateTrajectory,
+                                                   world=args.world)
             config['identifyGravityParamsOnly'] = old_gravity
         else:
-            idf = Identification(config, config['urdf'], urdf_file_real=None, measurements_files=None,
-                                 regressor_file=None, validation_file=None)
-            trajectoryOptimizer = TrajectoryOptimizer(config, idf, model, simulation_func=simulateTrajectory, world=args.world)
-
+            idf = Identification(config,
+                                 config['urdf'],
+                                 urdf_file_real=None,
+                                 measurements_files=None,
+                                 regressor_file=None,
+                                 validation_file=None)
+            trajectoryOptimizer = TrajectoryOptimizer(config,
+                                                      idf,
+                                                      model,
+                                                      simulation_func=simulateTrajectory,
+                                                      world=args.world)
+		# compute trajectory
         trajectory = trajectoryOptimizer.optimizeTrajectory()
         config['simulateTorques'] = old_sim
     else:
         # use some random params
         print("no optimized trajectory found, generating random one")
-        trajectory = PulsedTrajectory(config['num_dofs'], use_deg=config['useDeg']).initWithRandomParams()
+        trajectory = PulsedTrajectory( config['num_dofs'],
+                                       use_deg=config['useDeg']).initWithRandomParams()
         print("a {}".format([t_a.tolist() for t_a in trajectory.a]))
         print("b {}".format([t_b.tolist() for t_b in trajectory.b]))
         print("q {}".format(trajectory.q.tolist()))
@@ -87,6 +118,9 @@ def main():
         np.savez(traj_file, use_deg=trajectory.use_deg, static=False, a=trajectory.a, b=trajectory.b,
                  q=trajectory.q, nf=trajectory.nf, wf=trajectory.w_f_global)
 
+#==============================
+# Default execution entrypoint
+#==============================
 if __name__ == '__main__':
     logging.basicConfig()
     main()
